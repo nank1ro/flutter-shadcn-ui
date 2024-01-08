018e2c6dbbc8efc7334a279de7802a6a0c041c6d
Alexandru Mariuti <alex@mariuti.com>
2024-01-08 16:18:47 +0100

    improve button component

--- lib/src/components/button.dart
+++ lib/src/components/button.dart
@@ -21,7 +21,8 @@ enum ShadcnButtonSize {
 class ShadcnButton extends StatefulWidget {
   const ShadcnButton({
     super.key,
-    required this.text,
+    this.text,
+    this.icon,
     this.onPressed,
     this.style,
     this.size = ShadcnButtonSize.$default,
@@ -30,15 +31,17 @@ class ShadcnButton extends StatefulWidget {
   const ShadcnButton.raw({
     super.key,
     required this.variant,
-    required this.text,
     required this.size,
+    this.text,
+    this.icon,
     this.onPressed,
     this.style,
   });
 
   const ShadcnButton.destructive({
     super.key,
-    required this.text,
+    this.text,
+    this.icon,
     this.onPressed,
     this.style,
     this.size = ShadcnButtonSize.$default,
@@ -46,7 +49,8 @@ class ShadcnButton extends StatefulWidget {
 
   const ShadcnButton.outline({
     super.key,
-    required this.text,
+    this.text,
+    this.icon,
     this.onPressed,
     this.style,
     this.size = ShadcnButtonSize.$default,
@@ -54,7 +58,8 @@ class ShadcnButton extends StatefulWidget {
 
   const ShadcnButton.secondary({
     super.key,
-    required this.text,
+    this.text,
+    this.icon,
     this.onPressed,
     this.style,
     this.size = ShadcnButtonSize.$default,
@@ -62,7 +67,8 @@ class ShadcnButton extends StatefulWidget {
 
   const ShadcnButton.ghost({
     super.key,
-    required this.text,
+    this.text,
+    this.icon,
     this.onPressed,
     this.style,
     this.size = ShadcnButtonSize.$default,
@@ -74,10 +80,12 @@ class ShadcnButton extends StatefulWidget {
     this.onPressed,
     this.style,
     this.size = ShadcnButtonSize.$default,
-  }) : variant = ShadcnButtonVariant.link;
+  })  : variant = ShadcnButtonVariant.link,
+        icon = null;
 
   final VoidCallback? onPressed;
-  final String text;
+  final Widget? icon;
+  final Widget? text;
   final TextStyle? style;
   final ShadcnButtonVariant variant;
   final ShadcnButtonSize size;
@@ -88,6 +96,20 @@ class ShadcnButton extends StatefulWidget {
 
 class _ShadcnButtonState extends State<ShadcnButton> {
   final isHovered = ValueNotifier(false);
+
+  void assertCheckHasTextOrIcon() {
+    assert(
+      widget.text != null || widget.icon != null,
+      'Either text or icon must be provided',
+    );
+  }
+
+  void assertCheckIconWithIconSize() {
+    assert(
+      widget.icon == null || widget.size == ShadcnButtonSize.icon,
+      'Icon can only be provided if size is set to icon',
+    );
+  }
 
   @override
   void dispose() {
@@ -101,6 +123,13 @@ class _ShadcnButtonState extends State<ShadcnButton> {
       ShadcnButtonSize.sm => 36,
       ShadcnButtonSize.lg => 44,
       ShadcnButtonSize.icon => 40,
+    };
+  }
+
+  double? get width {
+    return switch (widget.size) {
+      ShadcnButtonSize.icon => 40,
+      _ => null,
     };
   }
 
@@ -120,9 +149,7 @@ class _ShadcnButtonState extends State<ShadcnButton> {
       ShadcnButtonVariant.$default => theme.primary,
       ShadcnButtonVariant.destructive => theme.destructive,
       ShadcnButtonVariant.secondary => theme.secondary,
-      ShadcnButtonVariant.outline => null,
-      ShadcnButtonVariant.ghost => null,
-      ShadcnButtonVariant.link => null,
+      _ => null,
     };
   }
 
@@ -130,10 +157,10 @@ class _ShadcnButtonState extends State<ShadcnButton> {
     return switch (widget.variant) {
       ShadcnButtonVariant.$default => theme.primary.withOpacity(.9),
       ShadcnButtonVariant.destructive => theme.destructive.withOpacity(.9),
-      ShadcnButtonVariant.secondary => theme.secondary,
+      ShadcnButtonVariant.secondary => theme.secondary.withOpacity(.8),
       ShadcnButtonVariant.ghost => theme.accent,
-      ShadcnButtonVariant.outline => null,
-      ShadcnButtonVariant.link => null,
+      ShadcnButtonVariant.outline => theme.accent,
+      _ => null,
     };
   }
 
@@ -144,7 +171,7 @@ class _ShadcnButtonState extends State<ShadcnButton> {
       ShadcnButtonVariant.outline => theme.accentForeground,
       ShadcnButtonVariant.secondary => theme.secondaryForeground,
       ShadcnButtonVariant.link => theme.primary,
-      ShadcnButtonVariant.ghost => null,
+      ShadcnButtonVariant.ghost => theme.primary,
     };
   }
 
@@ -159,36 +186,68 @@ class _ShadcnButtonState extends State<ShadcnButton> {
     };
   }
 
+  TextDecoration? textDecoration({required bool hovered}) {
+    return switch (widget.variant) {
+      ShadcnButtonVariant.link => hovered ? TextDecoration.underline : null,
+      _ => null,
+    };
+  }
+
+  BoxBorder? border(ShadcnThemeData theme) {
+    return switch (widget.variant) {
+      ShadcnButtonVariant.outline => Border.all(color: theme.input),
+      _ => null,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     assert(debugCheckHasShadcnTheme(context));
+    assertCheckHasTextOrIcon();
+    assertCheckIconWithIconSize();
+
     final shadcnTheme = ShadcnTheme.of(context);
     final effectiveStyle = widget.style ?? const TextStyle(fontSize: 14);
+
+    // Applies the foreground color filter to the icon if provided
+    var icon = widget.icon;
+    if (icon != null && foreground(shadcnTheme) != null) {
+      icon = ColorFiltered(
+        colorFilter: ColorFilter.mode(
+          foreground(shadcnTheme)!,
+          BlendMode.srcIn,
+        ),
+        child: icon,
+      );
+    }
+
     return Opacity(
       opacity: widget.onPressed == null ? .5 : 1,
-      child: ValueListenableBuilder(
-        valueListenable: isHovered,
-        builder: (context, hovered, child) {
-          return Container(
-            height: height,
-            decoration: BoxDecoration(
-              color: hovered
-                  ? hoverBackground(shadcnTheme)
-                  : background(shadcnTheme),
-              borderRadius: const BorderRadius.all(Radius.circular(6)),
-            ),
-            padding: padding,
-            child: child,
-          );
-        },
-        child: MouseRegion(
-          onEnter: (_) => isHovered.value = true,
-          onExit: (_) => isHovered.value = false,
-          cursor: widget.onPressed == null
-              ? MouseCursor.defer
-              : SystemMouseCursors.click,
-          child: GestureDetector(
-            onTap: widget.onPressed,
+      child: MouseRegion(
+        onEnter: (_) => isHovered.value = true,
+        onExit: (_) => isHovered.value = false,
+        cursor: widget.onPressed == null
+            ? MouseCursor.defer
+            : SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: widget.onPressed,
+          child: ValueListenableBuilder(
+            valueListenable: isHovered,
+            builder: (context, hovered, child) {
+              return Container(
+                height: height,
+                width: width,
+                decoration: BoxDecoration(
+                  color: hovered
+                      ? hoverBackground(shadcnTheme)
+                      : background(shadcnTheme),
+                  borderRadius: const BorderRadius.all(Radius.circular(6)),
+                  border: border(shadcnTheme),
+                ),
+                padding: padding,
+                child: child,
+              );
+            },
             child: Column(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -196,20 +255,24 @@ class _ShadcnButtonState extends State<ShadcnButton> {
                 ValueListenableBuilder(
                   valueListenable: isHovered,
                   builder: (context, hovered, _) {
-                    return Text(
-                      widget.text,
-                      style: effectiveStyle.copyWith(
-                        color: hovered
-                            ? hoverForeground(shadcnTheme)
-                            : foreground(shadcnTheme),
-                        decoration:
-                            widget.variant == ShadcnButtonVariant.link &&
-                                    hovered
-                                ? TextDecoration.underline
-                                : null,
-                        decorationColor: foreground(shadcnTheme),
-                      ),
-                      textAlign: TextAlign.center,
+                    return Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (icon != null) icon,
+                        if (widget.text != null)
+                          DefaultTextStyle(
+                            style: effectiveStyle.copyWith(
+                              color: hovered
+                                  ? hoverForeground(shadcnTheme)
+                                  : foreground(shadcnTheme),
+                              decoration: textDecoration(hovered: hovered),
+                              decorationColor: foreground(shadcnTheme),
+                              decorationStyle: TextDecorationStyle.solid,
+                            ),
+                            textAlign: TextAlign.center,
+                            child: widget.text!,
+                          ),
+                      ],
                     );
                   },
                 ),
