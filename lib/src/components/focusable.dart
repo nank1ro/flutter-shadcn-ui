476d20a90c28a11c1fa95fe0e91fd5b2ff60ecf3
Alexandru Mariuti <alex@mariuti.com>
2024-01-26 17:20:48 +0100

    add focusable widget

---
+++ lib/src/components/focusable.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/widgets.dart';
+
+typedef FocusWidgetBuilder = Widget Function(
+  BuildContext context,
+  bool focused,
+);
+
+class ShadFocused extends StatefulWidget {
+  const ShadFocused({
+    super.key,
+    required this.builder,
+    this.focusNode,
+    this.canRequestFocus = true,
+    this.autofocus = false,
+    this.child,
+  });
+
+  final bool canRequestFocus;
+  final bool autofocus;
+  final FocusNode? focusNode;
+  final FocusWidgetBuilder builder;
+  final Widget? child;
+
+  @override
+  State<ShadFocused> createState() => _ShadFocusedState();
+}
+
+class _ShadFocusedState extends State<ShadFocused> {
+  FocusNode? _internal;
+
+  final isFocused = ValueNotifier(false);
+
+  FocusNode get focusNode => widget.focusNode ?? _internal!;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.focusNode == null) _internal = FocusNode();
+    isFocused.value = focusNode.hasFocus;
+  }
+
+  @override
+  void dispose() {
+    _internal?.dispose();
+    isFocused.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+      autofocus: widget.autofocus,
+      canRequestFocus: widget.canRequestFocus,
+      onFocusChange: (value) => isFocused.value = value,
+      focusNode: focusNode,
+      child: ValueListenableBuilder(
+        valueListenable: isFocused,
+        builder: (context, value, child) => widget.builder(context, value),
+        child: widget.child,
+      ),
+    );
+  }
+}
