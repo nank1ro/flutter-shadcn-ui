b8218b017deb60c518b373607c3ee64e13f50728
Alexandru Mariuti <alex@mariuti.com>
2024-01-26 17:05:26 +0100

    update examples

--- example/lib/pages/select.dart
+++ example/lib/pages/select.dart
@@ -1,26 +1,44 @@
 import 'package:example/common/base_scaffold.dart';
+import 'package:example/common/properties/bool_property.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-class SelectPage extends StatelessWidget {
+class SelectPage extends StatefulWidget {
   const SelectPage({super.key});
 
+  @override
+  State<SelectPage> createState() => _SelectPageState();
+}
+
+class _SelectPageState extends State<SelectPage> {
+  bool enabled = true;
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
       appBarTitle: 'Select',
+      editable: [
+        MyBoolProperty(
+          label: 'Enabled',
+          value: enabled,
+          onChanged: (value) => setState(() => enabled = value),
+        ),
+      ],
       children: [
-        ShadcnSelect(
-          placeholder: const Text('Select a fruit'),
-          options: const [
-            ShadcnOption(value: 'apple', child: Text('Apple')),
-            ShadcnOption(value: 'banana', child: Text('Banana')),
-            ShadcnOption(value: 'blueberry', child: Text('Blueberry')),
-            ShadcnOption(value: 'grapes', child: Text('Grapes')),
-            ShadcnOption(value: 'pineapple', child: Text('Pineapple')),
-          ],
-          selectedOptionBuilder: (context, value) => Text(value),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: 180),
+          child: ShadcnSelect(
+            enabled: enabled,
+            placeholder: const Text('Select a fruit'),
+            options: const [
+              ShadcnOption(value: 'Apple', child: Text('Apple')),
+              ShadcnOption(value: 'Banana', child: Text('Banana')),
+              ShadcnOption(value: 'Blueberry', child: Text('Blueberry')),
+              ShadcnOption(value: 'Grapes', child: Text('Grapes')),
+              ShadcnOption(value: 'Pineapple', child: Text('Pineapple')),
+            ],
+            selectedOptionBuilder: (context, value) => Text(value),
+          ),
         ),
       ],
     );
