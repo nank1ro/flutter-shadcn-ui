fe939701f28f3096d8862e1c1327b028693ff4e9
Alexandru Mariuti <alex@mariuti.com>
2024-01-27 15:22:57 +0100

    add focused edit option to select example

--- example/lib/pages/select.dart
+++ example/lib/pages/select.dart
@@ -13,6 +13,13 @@ class SelectPage extends StatefulWidget {
 
 class _SelectPageState extends State<SelectPage> {
   bool enabled = true;
+  final focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,22 +32,31 @@ class _SelectPageState extends State<SelectPage> {
           value: enabled,
           onChanged: (value) => setState(() => enabled = value),
         ),
+        MyBoolProperty(
+          label: 'Focused',
+          value: focusNode.hasFocus,
+          onChanged: (value) => setState(
+            () => value ? focusNode.requestFocus() : focusNode.unfocus(),
+          ),
+        ),
       ],
       children: [
         ConstrainedBox(
           constraints: const BoxConstraints(minWidth: 180),
           child: ShadcnSelect<String>(
             enabled: enabled,
+            focusNode: focusNode,
             placeholder: const Text('Select a fruit'),
             options: [
               Padding(
-                padding: const EdgeInsets.only(left: 32, top: 6, bottom: 6),
+                padding: const EdgeInsets.fromLTRB(32, 6, 6, 6),
                 child: Text(
                   'Fruits',
                   style: theme.textTheme.muted.copyWith(
                     fontWeight: FontWeight.w600,
                     color: theme.colorScheme.popoverForeground,
                   ),
+                  textAlign: TextAlign.start,
                 ),
               ),
               const ShadcnOption(value: 'Apple', child: Text('Apple')),
