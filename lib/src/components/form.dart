12fb12dff3eb5cb76c8aa1f70523dfb8976d775c
Alexandru Mariuti <alex@mariuti.com>
2024-02-01 18:38:04 +0100

    feat/form

---
+++ lib/src/components/form.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+
+typedef ShadFormFields
+    = Map<String, ShadFormFieldState<ShadField<dynamic>, dynamic>>;
+
+class ShadForm extends StatefulWidget {
+  const ShadForm({
+    super.key,
+    required this.child,
+    this.canPop,
+    this.onPopInvoked,
+    this.autovalidateMode,
+  });
+
+  final bool? canPop;
+  final void Function(bool)? onPopInvoked;
+  final AutovalidateMode? autovalidateMode;
+  final Widget child;
+
+  @override
+  State<ShadForm> createState() => ShadFormState();
+
+  static ShadFormState of(BuildContext context) {
+    final state = maybeOf(context);
+    assert(state != null, 'No ShadFormState found in context');
+    return state!;
+  }
+
+  static ShadFormState? maybeOf(BuildContext context) =>
+      context.getElementForInheritedWidgetOfExactType<ShadFormScope>()?.widget
+          as ShadFormState?;
+}
+
+class ShadFormState extends State<ShadForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final ShadFormFields _fields = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      autovalidateMode: widget.autovalidateMode,
+      onPopInvoked: widget.onPopInvoked,
+      canPop: widget.canPop,
+      child: ShadFormScope(
+        formState: this,
+        child: FocusTraversalGroup(
+          policy: WidgetOrderTraversalPolicy(),
+          child: widget.child,
+        ),
+      ),
+    );
+  }
+}
+
+class ShadFormScope extends InheritedWidget {
+  const ShadFormScope({
+    super.key,
+    required super.child,
+    required ShadFormState formState,
+  }) : _formState = formState;
+
+  final ShadFormState _formState;
+
+  /// The [Form] associated with this widget.
+  ShadForm get form => _formState.widget;
+
+  @override
+  bool updateShouldNotify(ShadFormScope oldWidget) =>
+      oldWidget._formState != _formState;
+}
