499fc87eba335e9837679c51c534e46336ff75e9
Alexandru Mariuti <alex@mariuti.com>
2024-05-13 16:06:12 +0200

    chore/fix lints

--- lib/src/components/accordion.dart
+++ lib/src/components/accordion.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:shadcn_ui/src/components/image.dart';
