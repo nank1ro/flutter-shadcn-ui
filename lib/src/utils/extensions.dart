7bdf4264752b5e879a4c1f56fbe7858ec105acd0
Alexandru Mariuti <alex@mariuti.com>
2024-06-21 15:54:58 +0200

    chore: add breakpoint extension

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:shadcn_ui/src/theme/theme.dart';
+import 'package:shadcn_ui/src/utils/responsive.dart';
+
 extension ShadDuration on Duration {
   Duration operator +(Duration other) {
     return Duration(
@@ -33,5 +37,13 @@ extension ShadDuration on Duration {
     return Duration(
       microseconds: inMicroseconds ~/ by,
     );
+  }
+}
+
+extension ShadBreakpoints on BuildContext {
+  ShadBreakpoint get breakpoint {
+    final width = MediaQuery.sizeOf(this).width;
+    final breakpoints = ShadTheme.of(this).breakpoints;
+    return breakpoints.fromWidth(width);
   }
 }
