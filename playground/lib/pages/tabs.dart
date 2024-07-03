589690cee3cc6336892a2540506f66e85c73787e
Alexandru Mariuti <alex@mariuti.com>
2024-07-03 16:58:16 +0200

    add to pg

---
+++ playground/lib/pages/tabs.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: ShadTabs<String>(
+            defaultValue: 'account',
+            tabBarConstraints: const BoxConstraints(maxWidth: 400),
+            contentConstraints: const BoxConstraints(maxWidth: 400),
+            tabs: [
+              ShadTab(
+                value: 'account',
+                text: const Text('Account'),
+                content: ShadCard(
+                  title: const Text('Account'),
+                  description: const Text(
+                      "Make changes to your account here. Click save when you're done."),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(height: 16),
+                      ShadInputFormField(
+                        label: const Text('Name'),
+                        initialValue: 'Ale',
+                      ),
+                      const SizedBox(height: 8),
+                      ShadInputFormField(
+                        label: const Text('Username'),
+                        initialValue: 'nank1ro',
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                  footer: const ShadButton(text: Text('Save changes')),
+                ),
+              ),
+              ShadTab(
+                value: 'password',
+                text: const Text('Password'),
+                content: ShadCard(
+                  title: const Text('Password'),
+                  description: const Text(
+                      "Change your password here. After saving, you'll be logged out."),
+                  content: Column(
+                    children: [
+                      const SizedBox(height: 16),
+                      ShadInputFormField(
+                        label: const Text('Current password'),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 8),
+                      ShadInputFormField(
+                        label: const Text('New password'),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                  footer: const ShadButton(text: Text('Save password')),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
