fa916030e9edee76320bb79f3696dacc05c9f26d
Alexandru Mariuti <alex@mariuti.com>
2024-09-24 11:50:06 +0100

    docs/add site to docs template

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -27,7 +27,7 @@ export default defineConfig({
         }, {
           label: 'pub.dev',
           link: 'https://pub.dev/packages/shadcn_ui',
-          attrs: { target: '_blank' },
+          attrs: { target: '_blank', rel: 'noopener noreferrer' },
         },
         {
           label: 'Flutter Shadcn UI',
@@ -52,6 +52,19 @@ export default defineConfig({
             },
           ],
         },
+        {
+          label: 'Apps created with Flutter Shadcn UI',
+          collapsed: false,
+          items: [
+            {
+              label: 'Notes Calculator',
+              link: 'https://notescalculator.com',
+
+              attrs: { target: '_blank' },
+            },
+          ],
+
+        }
       ],
     }),
   ],
