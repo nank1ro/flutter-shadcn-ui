f6b5c296bd73d6bf3f302edfce1ef3465fa82cef
Alexandru Mariuti <alex@mariuti.com>
2024-10-23 09:42:31 +0100

    wip

--- example/lib/pages/date_picker.dart
+++ example/lib/pages/date_picker.dart
@@ -1,19 +1,36 @@
 import 'package:example/common/base_scaffold.dart';
+import 'package:example/common/properties/bool_property.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-class DatePickerPage extends StatelessWidget {
+class DatePickerPage extends StatefulWidget {
   const DatePickerPage({super.key});
+
+  @override
+  State<DatePickerPage> createState() => _DatePickerPageState();
+}
+
+class _DatePickerPageState extends State<DatePickerPage> {
+  bool closeOnSelection = false;
 
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
       appBarTitle: 'DatePicker',
+      editable: [
+        MyBoolProperty(
+          label: 'closeOnSelection',
+          value: closeOnSelection,
+          onChanged: (value) => setState(() => closeOnSelection = value),
+        ),
+      ],
       children: [
         ConstrainedBox(
           constraints: const BoxConstraints(maxWidth: 600),
-          child: const ShadDatePicker(),
+          child: ShadDatePicker(
+            closeOnSelection: closeOnSelection,
+          ),
         ),
       ],
     );
--- lib/src/components/date_picker.dart
+++ lib/src/components/date_picker.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:shadcn_ui/src/components/button.dart';
+import 'package:shadcn_ui/src/components/calendar.dart';
 import 'package:shadcn_ui/src/components/popover.dart';
+import 'package:shadcn_ui/src/theme/components/decorator.dart';
+import 'package:shadcn_ui/src/theme/theme.dart';
+
+import '../../shadcn_ui.dart';
 
 class ShadDatePicker extends StatefulWidget {
   const ShadDatePicker({
     super.key,
     this.popoverController,
+    this.selected,
+    this.closeOnSelection,
   });
 
   final ShadPopoverController? popoverController;
+  final DateTime? selected;
+  final bool? closeOnSelection;
 
   @override
   State<ShadDatePicker> createState() => _ShadDatePickerState();
 }
 
 class _ShadDatePickerState extends State<ShadDatePicker> {
+  late DateTime? selected = widget.selected;
   ShadPopoverController? _popoverController;
 
   ShadPopoverController get popoverController =>
@@ -36,14 +46,34 @@ class _ShadDatePickerState extends State<ShadDatePicker> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = ShadTheme.of(context);
+
+    final isSelected = selected != null;
+
     return ShadPopover(
       controller: popoverController,
       popover: (context) {
-        return ShadCalendar();
+        return ShadCalendar(
+          selected: selected,
+          decoration: ShadDecoration.none,
+          onChanged: (selected) {
+            setState(() => this.selected = selected);
+            if (true == widget.closeOnSelection) {
+              popoverController.hide();
+            }
+          },
+        );
       },
       child: ShadButton.raw(
         variant: ShadButtonVariant.outline,
-        child: Text('press me'),
+        width: 276,
+        mainAxisAlignment: MainAxisAlignment.start,
+        applyIconColorFilter: false,
+        icon: ShadImage.square(LucideIcons.calendar,
+            size: 16, color: theme.colorScheme.mutedForeground),
+        child: isSelected
+            ? Text(selected.toString())
+            : Text('Pick a date', style: theme.textTheme.muted),
         onPressed: () {
           popoverController.toggle();
         },
