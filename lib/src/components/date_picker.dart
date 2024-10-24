7ee06d57050fa49384681481c7671c7e12a3d154
Alexandru Mariuti <alex@mariuti.com>
2024-10-24 16:22:32 +0100

    feat: allow deselection and date format with suffix

--- lib/src/components/date_picker.dart
+++ lib/src/components/date_picker.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:shadcn_ui/src/components/button.dart';
 import 'package:shadcn_ui/src/components/calendar.dart';
+import 'package:shadcn_ui/src/components/image.dart';
 import 'package:shadcn_ui/src/components/popover.dart';
 import 'package:shadcn_ui/src/theme/components/decorator.dart';
 import 'package:shadcn_ui/src/theme/theme.dart';
-
-import '../../shadcn_ui.dart';
+import 'package:shadcn_ui/src/utils/extensions/date_time.dart';
 
 class ShadDatePicker extends StatefulWidget {
   const ShadDatePicker({
@@ -13,11 +15,15 @@ class ShadDatePicker extends StatefulWidget {
     this.popoverController,
     this.selected,
     this.closeOnSelection,
+    this.formatDate,
+    this.allowDeselection,
   });
 
   final ShadPopoverController? popoverController;
   final DateTime? selected;
   final bool? closeOnSelection;
+  final String Function(DateTime)? formatDate;
+  final bool? allowDeselection;
 
   @override
   State<ShadDatePicker> createState() => _ShadDatePickerState();
@@ -44,17 +50,28 @@ class _ShadDatePickerState extends State<ShadDatePicker> {
     super.dispose();
   }
 
+  String defaultDateFormat(DateTime date) {
+    final dayWithSuffix = date.getDayWithSuffix();
+    final month = DateFormat('MMMM').format(date); // October
+    final year = DateFormat('y').format(date); // 2024
+    return '$month $dayWithSuffix, $year'; // October 1st, 2024
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = ShadTheme.of(context);
 
     final isSelected = selected != null;
 
+    final effectiveFormatDate = widget.formatDate ?? defaultDateFormat;
+    final effectiveAllowDeselection = widget.allowDeselection ?? true;
+
     return ShadPopover(
       controller: popoverController,
       popover: (context) {
         return ShadCalendar(
           selected: selected,
+          allowDeselection: effectiveAllowDeselection,
           decoration: ShadDecoration.none,
           onChanged: (selected) {
             setState(() => this.selected = selected);
@@ -69,10 +86,15 @@ class _ShadDatePickerState extends State<ShadDatePicker> {
         width: 276,
         mainAxisAlignment: MainAxisAlignment.start,
         applyIconColorFilter: false,
-        icon: ShadImage.square(LucideIcons.calendar,
-            size: 16, color: theme.colorScheme.mutedForeground),
+        icon: ShadImage.square(
+          LucideIcons.calendar,
+          size: 16,
+          color: isSelected
+              ? theme.colorScheme.foreground
+              : theme.colorScheme.mutedForeground,
+        ),
         child: isSelected
-            ? Text(selected.toString())
+            ? Text(effectiveFormatDate(selected!))
             : Text('Pick a date', style: theme.textTheme.muted),
         onPressed: () {
           popoverController.toggle();
