06a36a12201a6d0c1e2c05ae55ffecd4c1c2503b
Alexandru Mariuti <alex@mariuti.com>
2024-11-27 10:59:12 +0100

    remove unused imports

--- playground/lib/main.dart
+++ playground/lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:js_interop';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:playground/pages/accordion.dart';
