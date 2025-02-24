8267d9a2d604a328290577f9267e353a2c61d692
Alexandru Mariuti <alex@mariuti.com>
2025-02-24 16:32:07 +0100

    chore: comment resizable

--- lib/src/theme/components/resizable.dart
+++ lib/src/theme/components/resizable.dart
@@ -24,19 +24,47 @@ class ShadResizableTheme {
   });
 
   final bool merge;
+
+  /// {@macro ShadResizablePanelGroup.mainAxisAlignment}
   final MainAxisAlignment? mainAxisAlignment;
+
+  /// {@macro ShadResizablePanelGroup.crossAxisAlignment}
   final CrossAxisAlignment? crossAxisAlignment;
+
+  /// {@macro ShadResizablePanelGroup.mainAxisSize}
   final MainAxisSize? mainAxisSize;
+
+  /// {@macro ShadResizablePanelGroup.textDirection}
   final TextDirection? textDirection;
+
+  /// {@macro ShadResizablePanelGroup.verticalDirection}
   final VerticalDirection? verticalDirection;
+
+  /// {@macro ShadResizablePanelGroup.showHandle}
   final bool? showHandle;
+
+  /// {@macro ShadResizablePanelGroup.handleIconData}
   final IconData? handleIconData;
+
+  /// {@macro ShadResizablePanelGroup.dividerSize}
   final double? dividerSize;
+
+  /// {@macro ShadResizablePanelGroup.resetOnDoubleTap}
   final bool? resetOnDoubleTap;
+
+  /// {@macro ShadResizablePanelGroup.handleDecoration}
   final ShadDecoration? handleDecoration;
+
+  /// {@macro ShadResizablePanelGroup.handlePadding}
   final EdgeInsets? handlePadding;
+
+  /// {@macro ShadResizablePanelGroup.handleSize}
   final double? handleSize;
+
+  /// {@macro ShadResizablePanelGroup.dividerThickness}
   final double? dividerThickness;
+
+  /// {@macro ShadResizablePanelGroup.dividerColor}
   final Color? dividerColor;
 
   static ShadResizableTheme lerp(
