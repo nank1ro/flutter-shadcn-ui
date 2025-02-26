385aa893d03e966f4b2465e870f7cf8599f3a520
Alexandru Mariuti <alex@mariuti.com>
2025-02-26 13:35:24 +0100

    chore: remove fake tests

--- test/extra/diagnosticable_mixin.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-mixin DiagnosticableToStringMixin on Object {
-  @override
-  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
-    return super.toString();
-  }
-}
