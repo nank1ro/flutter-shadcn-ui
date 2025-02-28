d202dbca654c401952677ec4996a489bd55315fc
Alexandru Mariuti <alex@mariuti.com>
2025-02-28 18:06:27 +0100

    feat: menubar

--- example/lib/pages/menubar.dart
+++ example/lib/pages/menubar.dart
@@ -8,10 +8,151 @@ class MenubarPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BaseScaffold(
+    final theme = ShadTheme.of(context);
+    final square = SizedBox.square(
+      dimension: 16,
+      child: Center(
+        child: SizedBox.square(
+          dimension: 8,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: theme.colorScheme.foreground,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+      ),
+    );
+    final divider = ShadDivider.horizontal(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      color: theme.colorScheme.muted,
+    );
+
+    return BaseScaffold(
       appBarTitle: 'Menubar',
       children: [
-        ShadMenubar(),
+        ShadMenubar(
+          items: [
+            ShadMenubarItem(
+              items: [
+                const ShadContextMenuItem(
+                  child: Text('New Tab'),
+                ),
+                const ShadContextMenuItem(
+                  child: Text('New Window'),
+                ),
+                const ShadContextMenuItem(
+                  enabled: false,
+                  child: Text('New Incognito Window'),
+                ),
+                divider,
+                const ShadContextMenuItem(
+                  trailing: Icon(LucideIcons.chevronRight),
+                  items: [
+                    ShadContextMenuItem(
+                      child: Text('Email Link'),
+                    ),
+                    ShadContextMenuItem(
+                      child: Text('Messages'),
+                    ),
+                    ShadContextMenuItem(
+                      child: Text('Notes'),
+                    ),
+                  ],
+                  child: Text('Share'),
+                ),
+                divider,
+                const ShadContextMenuItem(
+                  child: Text('Print...'),
+                ),
+              ],
+              child: const Text('File'),
+            ),
+            ShadMenubarItem(
+              items: [
+                const ShadContextMenuItem(
+                  child: Text('Undo'),
+                ),
+                const ShadContextMenuItem(
+                  child: Text('Redo'),
+                ),
+                divider,
+                ShadContextMenuItem(
+                  trailing: const Icon(LucideIcons.chevronRight),
+                  items: [
+                    const ShadContextMenuItem(
+                      child: Text('Search the web'),
+                    ),
+                    divider,
+                    const ShadContextMenuItem(
+                      child: Text('Find...'),
+                    ),
+                    const ShadContextMenuItem(
+                      child: Text('Find Next'),
+                    ),
+                    const ShadContextMenuItem(
+                      child: Text('Find Previous'),
+                    ),
+                  ],
+                  child: const Text('Find'),
+                ),
+                divider,
+                const ShadContextMenuItem(
+                  child: Text('Cut'),
+                ),
+                const ShadContextMenuItem(
+                  child: Text('Copy'),
+                ),
+                const ShadContextMenuItem(
+                  child: Text('Paste'),
+                ),
+              ],
+              child: const Text('Edit'),
+            ),
+            ShadMenubarItem(
+              items: [
+                const ShadContextMenuItem.inset(
+                  child: Text('Always Show Bookmarks Bar'),
+                ),
+                const ShadContextMenuItem(
+                  leading: Icon(LucideIcons.check),
+                  child: Text('Always Show Full URLs'),
+                ),
+                divider,
+                const ShadContextMenuItem.inset(child: Text('Reload')),
+                const ShadContextMenuItem.inset(
+                  enabled: false,
+                  child: Text('Force Reload'),
+                ),
+                divider,
+                const ShadContextMenuItem.inset(
+                  child: Text('Toggle Full Screen'),
+                ),
+                divider,
+                const ShadContextMenuItem.inset(
+                  child: Text('Hide Sidebar'),
+                ),
+              ],
+              child: const Text('View'),
+            ),
+            ShadMenubarItem(items: [
+              const ShadContextMenuItem.inset(child: Text('Andy')),
+              ShadContextMenuItem(
+                leading: square,
+                child: const Text('Benoit'),
+              ),
+              const ShadContextMenuItem.inset(child: Text('Luis')),
+              divider,
+              const ShadContextMenuItem.inset(
+                child: Text('Edit...'),
+              ),
+              divider,
+              const ShadContextMenuItem.inset(
+                child: Text('Add Profile...'),
+              ),
+            ], child: const Text('Profiles')),
+          ],
+        ),
       ],
     );
   }
--- lib/src/components/menubar.dart
+++ lib/src/components/menubar.dart
@@ -1,10 +1,242 @@
-import 'package:flutter/widgets.dart';
+import 'dart:ui';
 
-class ShadMenubar extends StatelessWidget {
-  const ShadMenubar({super.key});
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:shadcn_ui/src/components/button.dart';
+import 'package:shadcn_ui/src/components/context_menu.dart';
+import 'package:shadcn_ui/src/components/popover.dart';
+import 'package:shadcn_ui/src/raw_components/portal.dart';
+import 'package:shadcn_ui/src/theme/components/decorator.dart';
+import 'package:shadcn_ui/src/theme/theme.dart';
+import 'package:shadcn_ui/src/utils/provider.dart';
+import 'package:shadcn_ui/src/utils/provider_index.dart';
+
+class ShadMenubarController extends ChangeNotifier {
+  int? _selectedIndex;
+
+  int? get selectedIndex => _selectedIndex;
+
+  set selectedIndex(int? value) {
+    if (_selectedIndex == value) return;
+    _selectedIndex = value;
+    notifyListeners();
+  }
+
+  final _focusNodes = <FocusNode>[];
+
+  FocusNode getFocusNodeForIndex(int index) {
+    if (index >= _focusNodes.length) {
+      for (var i = _focusNodes.length; i <= index; i++) {
+        _focusNodes.add(FocusNode());
+      }
+    }
+    return _focusNodes[index];
+  }
+
+  @override
+  void dispose() {
+    for (final node in _focusNodes) {
+      node.dispose();
+    }
+    _focusNodes.clear();
+
+    super.dispose();
+  }
+}
+
+class ShadMenubar extends StatefulWidget {
+  const ShadMenubar({
+    super.key,
+    required this.items,
+    this.radius,
+    this.controller,
+  });
+
+  final Iterable<Widget> items;
+  final BorderRadiusGeometry? radius;
+  final ShadMenubarController? controller;
+
+  @override
+  State<ShadMenubar> createState() => _ShadMenubarState();
+}
+
+class _ShadMenubarState extends State<ShadMenubar> {
+  int? focusedIndex;
+  ShadMenubarController? _controller;
+
+  ShadMenubarController get controller => widget.controller ?? _controller!;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.controller == null) {
+      _controller = ShadMenubarController();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Menubar');
+    final theme = ShadTheme.of(context);
+    final effectiveRadius = widget.radius ?? theme.radius;
+    return ShadProvider(
+      data: controller,
+      notifyUpdate: (oldState) {
+        return oldState.data != controller;
+      },
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: theme.colorScheme.border,
+          ),
+          borderRadius: effectiveRadius,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: widget.items
+                .mapIndexed(
+                  (i, item) => ShadIndexProvider(index: i, child: item),
+                )
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ShadMenubarItem extends StatefulWidget {
+  const ShadMenubarItem({
+    super.key,
+    required this.child,
+    required this.items,
+    this.constraints,
+    this.onHoverArea,
+    this.padding,
+    this.groupId,
+    this.effects,
+    this.shadows,
+    this.decoration,
+    this.filter,
+    this.controller,
+  });
+
+  final Widget child;
+
+  /// {@macro ShadContextMenu.items}
+  final List<Widget> items;
+
+  /// {@macro ShadContextMenu.constraints}
+  final BoxConstraints? constraints;
+
+  /// {@macro ShadContextMenu.onHoverArea}
+  final ValueChanged<bool>? onHoverArea;
+
+  /// {@macro ShadContextMenu.padding}
+  final EdgeInsetsGeometry? padding;
+
+  /// {@macro ShadMouseArea.groupId}
+  final Object? groupId;
+
+  /// {@macro ShadPopover.effects}
+  final List<Effect<dynamic>>? effects;
+
+  /// {@macro ShadPopover.shadows}
+  final List<BoxShadow>? shadows;
+
+  /// {@macro ShadPopover.decoration}
+  final ShadDecoration? decoration;
+
+  /// {@macro ShadPopover.filter}
+  final ImageFilter? filter;
+
+  /// {@macro ShadPopover.controller}
+  final ShadPopoverController? controller;
+
+  @override
+  State<ShadMenubarItem> createState() => _ShadMenubarItemState();
+}
+
+class _ShadMenubarItemState extends State<ShadMenubarItem> {
+  ShadPopoverController? _popoverController;
+
+  ShadPopoverController get popoverController =>
+      widget.controller ?? _popoverController!;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.controller == null) {
+      _popoverController = ShadPopoverController();
+    }
+  }
+
+  @override
+  void dispose() {
+    _popoverController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = ShadTheme.of(context);
+    final controller = context.watch<ShadMenubarController>();
+    final index = context.watch<ShadProviderIndex>().index;
+    final focusNode = controller.getFocusNodeForIndex(index);
+
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, child) {
+        final selected = controller.selectedIndex == index;
+        popoverController.setOpen(selected);
+        return ShadContextMenu(
+          anchor: const ShadAnchor(offset: Offset(-8, 8)),
+          controller: popoverController,
+          items: widget.items,
+          constraints: widget.constraints,
+          onHoverArea: widget.onHoverArea,
+          padding: widget.padding,
+          groupId: widget.groupId,
+          effects: widget.effects,
+          shadows: widget.shadows,
+          decoration: widget.decoration,
+          filter: widget.filter,
+          child: ShadButton.ghost(
+            height: 32,
+            backgroundColor: selected ? theme.colorScheme.accent : null,
+            focusNode: focusNode,
+            onFocusChange: (focused) {
+              if (!focused) return;
+              controller.selectedIndex = index;
+            },
+            onHoverChange: (hovered) {
+              if (!hovered) return;
+              controller.selectedIndex = index;
+            },
+            decoration: const ShadDecoration(
+              disableSecondaryBorder: true,
+            ),
+            onPressed: () {
+              if (!popoverController.isOpen && selected) {
+                popoverController.show();
+              } else {
+                controller.selectedIndex = selected ? null : index;
+              }
+            },
+            child: child,
+          ),
+        );
+      },
+      child: widget.child,
+    );
   }
 }
