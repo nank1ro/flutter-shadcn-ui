b73ffa2ad1afd65e8a56d6b196041c31bf1e2b9c
Alexandru Mariuti <alex@mariuti.com>
2025-03-07 12:07:22 +0100

    chore: CLI boilerplate

---
+++ shadcn/bin/shadcn.dart
@@ -0,0 +1,5 @@
+import 'package:shadcn/shadcn.dart' as shadcn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shadcn.calculate()}!');
+}
---
+++ shadcn/test/shadcn_test.dart
@@ -0,0 +1,8 @@
+import 'package:shadcn/shadcn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
