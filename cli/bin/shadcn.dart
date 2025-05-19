3ab585bf1e70efd6cd87ff971977981a1910f0e4
Alexandru Mariuti <alex@mariuti.com>
2025-05-19 18:54:30 +0800

    refactor: switch to cli_tools instead of args

--- cli/bin/shadcn.dart
+++ cli/bin/shadcn.dart
@@ -1,12 +1,59 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:cli_util/cli_logging.dart';
 import 'package:path/path.dart' as path;
 import 'package:shadcn/shadcn.dart';
+import 'package:cli_tools/cli_tools.dart' hide Logger;
 
-void main(List<String> arguments) {
+late Logger logger;
+
+enum Options implements OptionDefinition<String> {
+  component(
+    StringOption(
+      argName: 'component',
+      helpText: 'The component to add',
+      argPos: 0,
+      allowedValues: components,
+      mandatory: true,
+    ),
+  );
+
+  const Options(this.option);
+
+  @override
+  final ConfigOptionBase<String> option;
+}
+
+class AddComponentCommand extends BetterCommand<Options, void> {
+  AddComponentCommand() : super(options: Options.values);
+  // argParser.addOption(
+  //   'component',
+  //   aliases: [],
+  //   abbr: 'c',
+  //   help: 'The component to add',
+  //   mandatory: true,
+  //   allowed: components,
+  // );
+  // }
+  @override
+  String get name => 'add';
+
+  @override
+  String get description => 'Add a component to the project.';
+
+  @override
+  Future<void> runWithConfig(Configuration<Options> commandConfig) async {
+    final component = commandConfig.value(Options.component);
+    logger.progress('Adding component: $component');
+    logger.stdout('finish');
+  }
+}
+
+Future<int> main(List<String> arguments) async {
   final verbose = arguments.contains('-v');
-  final logger = Logger.verbose(logTime: verbose);
+  logger = Logger.verbose(logTime: verbose);
+
   if (!GitOperations.isGitRepository) {
     logger.stderr('This command must be run inside a git repository.');
     exit(1);
@@ -16,9 +63,8 @@ void main(List<String> arguments) {
   final pubspecFile = File(pubspecPath);
   if (!pubspecFile.existsSync()) {
     logger.stderr('No pubspec.yaml file found in the git repository.');
-    exit(1);
+    return 1;
   }
-  print('Git repository path: $repoPath');
 
   final pubspec = PubspecParser.parse(pubspecFile);
   final satifiesMinFlutterVersion = pubspec.satifiesMinFlutterVersion();
@@ -30,7 +76,14 @@ void main(List<String> arguments) {
       errorMessage += ' ${satifiesMinFlutterVersion.errorMessage!}';
     }
     logger.stderr(errorMessage);
-    exit(1);
+    return 1;
   }
-  print(pubspec);
+
+  final runner = BetterCommandRunner<OptionDefinition<dynamic>, dynamic>(
+    'shadcn',
+    'A CLI for installing Flutter shadcn components',
+    globalOptions: [StandardGlobalOption.quiet, StandardGlobalOption.verbose],
+  )..addCommand(AddComponentCommand());
+  await runner.run(arguments);
+  return 0;
 }
