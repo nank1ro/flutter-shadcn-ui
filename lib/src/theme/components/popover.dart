0ea858f695cf4c35200c0db25e4ab21056d981fc
Mouayad <muayad.perun@outlook.com>
2025-08-06 07:12:27 +0300

    refactor: popover build method
    - In `ShadPopover`:  clean the build method by moving theme-dependent computations from the `build` method to `didChangeDependencies`.
    - Fix documentation macros in `ShadPopoverTheme`.
    - Remove unnecessary `_popoverFocusScopeNode` var

--- lib/src/theme/components/popover.dart
+++ lib/src/theme/components/popover.dart
@@ -22,22 +22,22 @@ class ShadPopoverTheme {
 
   final bool merge;
 
-  /// {@macro popover.effects}
+  /// {@macro ShadPopover.effects}
   final List<Effect<dynamic>>? effects;
 
-  /// {@macro popover.shadows}
+  /// {@macro ShadPopover.shadows}
   final List<BoxShadow>? shadows;
 
-  /// {@macro popover.padding}
+  /// {@macro ShadPopover.padding}
   final EdgeInsetsGeometry? padding;
 
-  /// {@macro popover.decoration}
+  /// {@macro ShadPopover.decoration}
   final ShadDecoration? decoration;
 
-  /// {@macro popover.anchor}
+  /// {@macro ShadPopover.anchor}
   final ShadAnchorBase? anchor;
 
-  /// {@macro popover.filter}
+  /// {@macro ShadPopover.filter}
   final ImageFilter? filter;
 
   /// {@macro ShadPopover.duration}
