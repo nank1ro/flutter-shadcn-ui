df86caebc7de1025e47ead4aa94b68fcbbb3cbae
Alexandru Mariuti <alex@mariuti.com>
2025-09-29 16:20:30 +0200

    refactor: themes using theme generator

--- lib/src/theme/components/accordion.dart
+++ lib/src/theme/components/accordion.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:theme_extensions_builder_annotation/theme_extensions_builder_annotation.dart';
 
+part 'accordion.g.theme.dart';
+
+@themeGen
 @immutable
-class ShadAccordionTheme {
+class ShadAccordionTheme with _$ShadAccordionTheme {
   const ShadAccordionTheme({
-    this.merge = true,
     this.iconData,
     this.iconEffects,
     this.padding,
@@ -16,8 +19,6 @@ class ShadAccordionTheme {
     this.maintainState,
     this.effects,
   });
-
-  final bool merge;
 
   /// {@macro ShadAccordionItem.iconData}
   final IconData? iconData;
@@ -50,94 +51,6 @@ class ShadAccordionTheme {
     ShadAccordionTheme a,
     ShadAccordionTheme b,
     double t,
-  ) {
-    if (identical(a, b)) return a;
-    return ShadAccordionTheme(
-      merge: t < 0.5 ? a.merge : b.merge,
-      iconData: t < 0.5 ? a.iconData : b.iconData,
-      iconEffects: t < 0.5 ? a.iconEffects : b.iconEffects,
-      padding: EdgeInsetsGeometry.lerp(a.padding, b.padding, t),
-      underlineTitleOnHover:
-          t < 0.5 ? a.underlineTitleOnHover : b.underlineTitleOnHover,
-      titleStyle: TextStyle.lerp(a.titleStyle, b.titleStyle, t),
-      curve: t < 0.5 ? a.curve : b.curve,
-      duration: t < 0.5 ? a.duration : b.duration,
-      maintainState: t < 0.5 ? a.maintainState : b.maintainState,
-      effects: t < 0.5 ? a.effects : b.effects,
-    );
-  }
-
-  ShadAccordionTheme mergeWith(ShadAccordionTheme? other) {
-    if (other == null) return this;
-    if (!other.merge) return other;
-    return copyWith(
-      iconData: other.iconData,
-      iconEffects: other.iconEffects,
-      padding: other.padding,
-      underlineTitleOnHover: other.underlineTitleOnHover,
-      titleStyle: other.titleStyle,
-      curve: other.curve,
-      duration: other.duration,
-      maintainState: other.maintainState,
-      effects: other.effects,
-    );
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is ShadAccordionTheme &&
-        other.merge == merge &&
-        other.iconData == iconData &&
-        listEquals(other.iconEffects, iconEffects) &&
-        other.padding == padding &&
-        other.underlineTitleOnHover == underlineTitleOnHover &&
-        other.titleStyle == titleStyle &&
-        other.curve == curve &&
-        other.duration == duration &&
-        other.maintainState == maintainState &&
-        listEquals(other.effects, effects);
-  }
-
-  @override
-  int get hashCode {
-    return merge.hashCode ^
-        iconData.hashCode ^
-        iconEffects.hashCode ^
-        padding.hashCode ^
-        underlineTitleOnHover.hashCode ^
-        titleStyle.hashCode ^
-        curve.hashCode ^
-        duration.hashCode ^
-        maintainState.hashCode ^
-        effects.hashCode;
-  }
-
-  ShadAccordionTheme copyWith({
-    bool? merge,
-    IconData? iconData,
-    List<Effect<dynamic>>? iconEffects,
-    EdgeInsetsGeometry? padding,
-    bool? underlineTitleOnHover,
-    TextStyle? titleStyle,
-    Curve? curve,
-    Duration? duration,
-    bool? maintainState,
-    List<Effect<dynamic>>? effects,
-  }) {
-    return ShadAccordionTheme(
-      merge: merge ?? this.merge,
-      iconData: iconData ?? this.iconData,
-      iconEffects: iconEffects ?? this.iconEffects,
-      padding: padding ?? this.padding,
-      underlineTitleOnHover:
-          underlineTitleOnHover ?? this.underlineTitleOnHover,
-      titleStyle: titleStyle ?? this.titleStyle,
-      curve: curve ?? this.curve,
-      duration: duration ?? this.duration,
-      maintainState: maintainState ?? this.maintainState,
-      effects: effects ?? this.effects,
-    );
-  }
+  ) =>
+      _$ShadAccordionTheme.lerp(a, b, t);
 }
---
+++ lib/src/theme/components/accordion.g.theme.dart
@@ -0,0 +1,130 @@
+// dart format width=80
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, unused_element
+
+part of 'accordion.dart';
+
+// **************************************************************************
+// ThemeGenGenerator
+// **************************************************************************
+
+mixin _$ShadAccordionTheme {
+  bool get canMerge => true;
+
+  static ShadAccordionTheme? lerp(
+    ShadAccordionTheme? a,
+    ShadAccordionTheme? b,
+    double t,
+  ) {
+    if (identical(a, b)) {
+      return a;
+    }
+
+    return ShadAccordionTheme(
+      iconData: t < 0.5 ? a?.iconData : b?.iconData,
+      iconEffects: t < 0.5 ? a?.iconEffects : b?.iconEffects,
+      padding: EdgeInsetsGeometry.lerp(a?.padding, b?.padding, t),
+      underlineTitleOnHover:
+          t < 0.5 ? a?.underlineTitleOnHover : b?.underlineTitleOnHover,
+      titleStyle: TextStyle.lerp(a?.titleStyle, b?.titleStyle, t),
+      curve: t < 0.5 ? a?.curve : b?.curve,
+      duration: lerpDuration$(a?.duration, b?.duration, t),
+      maintainState: t < 0.5 ? a?.maintainState : b?.maintainState,
+      effects: t < 0.5 ? a?.effects : b?.effects,
+    );
+  }
+
+  ShadAccordionTheme copyWith({
+    IconData? iconData,
+    List<Effect<dynamic>>? iconEffects,
+    EdgeInsetsGeometry? padding,
+    bool? underlineTitleOnHover,
+    TextStyle? titleStyle,
+    Curve? curve,
+    Duration? duration,
+    bool? maintainState,
+    List<Effect<dynamic>>? effects,
+  }) {
+    final a = (this as ShadAccordionTheme);
+
+    return ShadAccordionTheme(
+      iconData: iconData ?? a.iconData,
+      iconEffects: iconEffects ?? a.iconEffects,
+      padding: padding ?? a.padding,
+      underlineTitleOnHover: underlineTitleOnHover ?? a.underlineTitleOnHover,
+      titleStyle: titleStyle ?? a.titleStyle,
+      curve: curve ?? a.curve,
+      duration: duration ?? a.duration,
+      maintainState: maintainState ?? a.maintainState,
+      effects: effects ?? a.effects,
+    );
+  }
+
+  ShadAccordionTheme merge(ShadAccordionTheme? other) {
+    final current = (this as ShadAccordionTheme);
+
+    if (other == null) {
+      return current;
+    }
+
+    if (!other.canMerge) {
+      return other;
+    }
+
+    return copyWith(
+      iconData: other.iconData,
+      iconEffects: other.iconEffects,
+      padding: other.padding,
+      underlineTitleOnHover: other.underlineTitleOnHover,
+      titleStyle:
+          current.titleStyle?.merge(other.titleStyle) ?? other.titleStyle,
+      curve: other.curve,
+      duration: other.duration,
+      maintainState: other.maintainState,
+      effects: other.effects,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+
+    final value = (this as ShadAccordionTheme);
+
+    return other is ShadAccordionTheme &&
+        other.iconData == value.iconData &&
+        other.iconEffects == value.iconEffects &&
+        other.padding == value.padding &&
+        other.underlineTitleOnHover == value.underlineTitleOnHover &&
+        other.titleStyle == value.titleStyle &&
+        other.curve == value.curve &&
+        other.duration == value.duration &&
+        other.maintainState == value.maintainState &&
+        other.effects == value.effects;
+  }
+
+  @override
+  int get hashCode {
+    final value = (this as ShadAccordionTheme);
+
+    return Object.hash(
+      runtimeType,
+      value.iconData,
+      value.iconEffects,
+      value.padding,
+      value.underlineTitleOnHover,
+      value.titleStyle,
+      value.curve,
+      value.duration,
+      value.maintainState,
+      value.effects,
+    );
+  }
+}
