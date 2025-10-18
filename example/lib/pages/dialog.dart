8ca90e77a57482edd9e1b7442d4d6f064ce3bf50
Mouayad <muayad.perun@outlook.com>
2025-10-18 08:29:57 +0300

    feat(example): add scrollable dialog

--- example/lib/pages/dialog.dart
+++ example/lib/pages/dialog.dart
@@ -6,6 +6,28 @@ final profile = [
   (title: 'Name', value: 'Alexandru'),
   (title: 'Username', value: 'nank1ro'),
 ];
+final terms = [
+  (
+    title: 'Acceptance of Terms',
+    description:
+        'By accessing and using this service, you accept and agree to be bound by the terms and provision of this agreement. If you do not agree to abide by the above, please do not use this service.'
+  ),
+  (
+    title: 'Use License',
+    description:
+        'Permission is granted to temporarily download one copy of the materials (information or software) on our service for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title.'
+  ),
+  (
+    title: 'Disclaimer',
+    description:
+        'The materials on our service are provided on an \'as is\' basis. We make no warranties, expressed or implied, and hereby disclaim and negate all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.',
+  ),
+  (
+    title: 'Limitations',
+    description:
+        'In no event shall we or our suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on our service, even if we or a ShadcnUi supplier has been advised of the possibility of such damages. Furthermore, you agree that ShadcnUi shall not be liable for direct, indirect, consequential, incidental, special, exemplary, or punitive damages.',
+  )
+];
 
 class DialogPage extends StatelessWidget {
   const DialogPage({super.key});
@@ -86,7 +108,84 @@ class DialogPage extends StatelessWidget {
             );
           },
         ),
+        ShadButton.outline(
+          child: const Text('Scrollable Content'),
+          onPressed: () {
+            showShadDialog(
+              context: context,
+              builder: (context) => const _ScrollableDialog(),
+            );
+          },
+        ),
       ],
+    );
+  }
+}
+
+class _ScrollableDialog extends StatefulWidget {
+  const _ScrollableDialog();
+
+  @override
+  State<_ScrollableDialog> createState() => _ScrollableDialogState();
+}
+
+class _ScrollableDialogState extends State<_ScrollableDialog> {
+  bool isActionsPinned = true;
+  bool isHeaderPinned = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = ShadTheme.of(context);
+    return ShadDialog(
+      gap: 15,
+      isHeaderPinned: isHeaderPinned,
+      isActionsPinned: isActionsPinned,
+      constraints: BoxConstraints(maxWidth: 500, maxHeight: 500),
+      title: const Text('Term of Service'),
+      description: const Text('Please read our terms of service carefully.'),
+      actionsMainAxisAlignment: MainAxisAlignment.spaceBetween,
+      actions: [
+        ShadSwitch(
+          value: isActionsPinned,
+          label: Text(
+            'Actions pinned',
+            style: theme.textTheme.small,
+          ),
+          onChanged: (value) => setState(() => isActionsPinned = value),
+        ),
+        ShadSwitch(
+          value: isHeaderPinned,
+          label: Text(
+            'Header pinned',
+            style: theme.textTheme.small,
+          ),
+          onChanged: (value) => setState(() => isHeaderPinned = value),
+        ),
+      ],
+      child: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          spacing: 15,
+          children: List.generate(terms.length, (index) {
+            final term = terms[index];
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${index + 1}. ${term.title}',
+                  style: theme.textTheme.list.copyWith(
+                    color: theme.colorScheme.foreground,
+                  ),
+                ),
+                Text(
+                  term.description,
+                  style: theme.textTheme.muted,
+                ),
+              ],
+            );
+          }),
+        ),
+      ),
     );
   }
 }
