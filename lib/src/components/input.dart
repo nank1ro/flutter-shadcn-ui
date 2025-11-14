d1d3e8d707ac5848ab323b97e1cf645f95010207
Alexandru Mariuti <alex@mariuti.com>
2025-11-14 15:48:59 +0100

    remove print

--- lib/src/components/input.dart
+++ lib/src/components/input.dart
@@ -984,7 +984,6 @@ class ShadInputState extends State<ShadInput>
                     focused: focused,
                     child: BoxyColumn(
                       mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         if (widget.top != null) widget.top!,
                         RawScrollbar(
@@ -1039,10 +1038,6 @@ class ShadInputState extends State<ShadInput>
                                                           context,
                                                           constraints,
                                                         ) {
-                                                          print(
-                                                            'constraints: $constraints',
-                                                          );
-
                                                           /// Fire onLineCountChange after the frame is rendered
                                                           /// This ensures that the line count is accurate, even for
                                                           /// resizes that happen outside of text changes.
