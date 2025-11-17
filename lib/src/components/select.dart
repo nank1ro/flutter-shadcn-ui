fe7c32cbf898d69f793f91d93a7178185679e6ba
Alexandru Mariuti <alex@mariuti.com>
2025-11-17 15:26:16 +0100

    chore: remove unnecessary import

--- lib/src/components/select.dart
+++ lib/src/components/select.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:collection/collection.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
