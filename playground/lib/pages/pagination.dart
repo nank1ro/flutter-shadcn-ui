7c5f9fad3e77929104a9542bd3b29346472411e8
Joseph Asare <joasare019@gmail.com>
2026-01-10 19:24:09 +0000

    Update playground/lib/pages/pagination.dart
    Co-authored-by: Alexandru Mariuti <alex@mariuti.com>

--- playground/lib/pages/pagination.dart
+++ playground/lib/pages/pagination.dart
@@ -31,12 +31,12 @@ class _PaginationExampleState extends State<PaginationExample> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 20,
       children: [
         Text(
           'Pagination Example',
           style: ShadTheme.of(context).textTheme.h2,
         ),
-        const SizedBox(height: 20),
         // Add your pagination widget implementation here
         ShadPagination(
           totalPages: 10,
