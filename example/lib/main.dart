eee028ac857f6701a812535f5a2cb430e985b204
Alexandru Mariuti <alex@mariuti.com>
2026-01-21 17:47:37 +0100

    revert main

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -163,15 +163,15 @@ class App extends StatelessWidget {
               // disableSecondaryBorder: true,
 
               // Example of extending the ShadTextTheme with a new custom style and name (see `/typography` page for usage example).
-              // textTheme: ShadTextTheme(
-              //   custom: {
-              //     'myCustomStyle': const TextStyle(
-              //       fontSize: 16,
-              //       fontWeight: FontWeight.w400,
-              //       color: Colors.blue,
-              //     ),
-              //   },
-              // ),
+              textTheme: ShadTextTheme(
+                custom: {
+                  'myCustomStyle': const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.blue,
+                  ),
+                },
+              ),
             ),
             darkTheme: ShadThemeData(
               brightness: Brightness.dark,
