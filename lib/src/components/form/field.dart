6fbd362c27432e4703bac230aea3fffb0fb4e1f4
Alexandru Mariuti <alex@mariuti.com>
2026-01-22 13:18:56 +0100

    fix

--- lib/src/components/form/field.dart
+++ lib/src/components/form/field.dart
@@ -199,6 +199,7 @@ class ShadFormBuilderFieldState<F extends ShadFormBuilderField<T>, T>
     if (widget.initialValue != null) return widget.initialValue;
 
     final value = _parentForm?.widget.initialValue[widget.id];
+    if (value == null) return null;
     if (widget.fromValueTransformer != null) {
       return widget.fromValueTransformer!(value);
     }
