157686550cad702ddd88e81578e7ce1a3d1eb3ee
Theo <34074097+theomarie@users.noreply.github.com>
2026-08-04 17:53:13 +0200

    fix(input): expose enabled state in semantics (#680)

--- lib/src/components/input.dart
+++ lib/src/components/input.dart
@@ -1137,7 +1137,7 @@ class ShadInputState extends State<ShadInput>
                   valueListenable: effectiveController,
                   builder: (context, textEditingValue, child) {
                     final Widget editableText;
-                    final rawEditableText = SizedBox(
+                    final rawEditableTextContent = SizedBox(
                       width: widget.editableTextSize?.width,
                       height: widget.editableTextSize?.height,
                       child: EditableText(
@@ -1230,6 +1230,10 @@ class ShadInputState extends State<ShadInput>
                         showCursor: widget.showCursor,
                         groupId: effectiveGroupId,
                       ),
+                    );
+                    final rawEditableText = Semantics(
+                      enabled: widget.enabled,
+                      child: rawEditableTextContent,
                     );
 
                     if (widget.onLineCountChange != null) {
